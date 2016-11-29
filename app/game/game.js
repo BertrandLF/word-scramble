e436267e642efd736fc268b81c4aa566f147b8ac
Bertrand Le Foulgoc <bertrandlf@gmail.com>
2016-11-29 16:23:02 +0100

    pick first word and evaluate user input

--- app/game/game.js
+++ app/game/game.js
@@ -13,5 +13,27 @@ function Words($firebaseArray) {
 }
 
 function GameCtrl($scope, words) {
-  this.list = words.all()
+  var storedWords = words.all()
+  var remainingWords = []
+
+  storedWords.$loaded().then(function () {
+    angular.forEach(storedWords, function (value, key) {
+      remainingWords.push(value)
+    })
+    var currentWord = remainingWords[0]
+    $scope.guess = currentWord.$value
+    $scope.solution = currentWord.$id
+  })
+
+  this.list = remainingWords
+  $scope.evaluateGuess = function (keyEvent) {
+    if (stringsMatch($scope.guessValue, $scope.solution)) {
+      $scope.message = "You win!"
+    } else {
+      $scope.message = ""
+    }
+  }
+}
+function stringsMatch(guess, solution) {
+  return guess.toUpperCase() === solution.toUpperCase()
 }
